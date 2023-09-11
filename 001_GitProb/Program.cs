6d561acf175304403e338d3575b52353990e1a23
Denis <Denis@DESKTOP-KM84P71>
2023-09-11 14:04:48 +0300

    Add project files.

---
+++ 001_GitProb/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _001_GitProb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            Console.WriteLine($"{a}*{a}={a+a}");
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}

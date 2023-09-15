5a11c4c5432163f1aa2e78c0b6796a8bbee7fb16
denism <denismasakov83@gmail.com>
2023-09-15 21:20:48 +0300

    added Logic

---
+++ 001_GitProb/Logic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_GitProb
+{
+    internal class Logic
+    {
+        public int PowerOf2(int a)
+        {
+            return a * a;
+        }
+    }
+}

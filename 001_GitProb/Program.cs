da8850ac91f6e7cab004f349c8083bf3b5e62b3e
denism <denismasakov83@gmail.com>
2023-09-15 21:14:11 +0300

    deleted strings

--- 001_GitProb/Program.cs
+++ 001_GitProb/Program.cs
@@ -5,10 +5,8 @@
         static void Main(string[] args)
         {
             int a = int.Parse(Console.ReadLine());
-            int b = 3;
-            Console.WriteLine($"{a}*{a}={a+a}");
-            Console.WriteLine("Hello, World!");
-            Console.ReadKey();
+            Console.WriteLine($"{a} * {a}={ a * a}");
+           
         }
     }
 }

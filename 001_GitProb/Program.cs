2ba310683a1a279443178370d952027292e9c355
denism <denismasakov83@gmail.com>
2023-09-22 18:26:07 +0300

    Class Program added CW(Hello)

--- 001_GitProb/Program.cs
+++ 001_GitProb/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             int a = int.Parse(Console.ReadLine());
-            Console.WriteLine($"{a} * {a} = {Logic.Power(a, 2)}");           
+            Console.WriteLine($"{a} * {a} = {Logic.Power(a, 2)}");
+            Console.WriteLine("Hello!");
         }
     }
 }
